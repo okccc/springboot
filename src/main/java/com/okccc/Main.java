4fa1d4f4a8cf7652b1694d5eb0e1e95a03e9d3b7
okccc <1573976179@qq.com>
2024-06-02 20:00:52 +0800

    SpringBoot开发流程

---
+++ src/main/java/com/okccc/Main.java
@@ -0,0 +1,16 @@
+package com.okccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: okccc
+ * @Date: 2023/12/30 16:52:01
+ * @Desc: 启动类
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
