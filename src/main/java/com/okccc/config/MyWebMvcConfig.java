220301a1c3abe48a388883cd6ebf863a56615932
okccc <1573976179@qq.com>
2024-06-18 10:42:59 +0800

    WebMvcConfigurer如何配置SpringMVC底层行为

--- src/main/java/com/okccc/config/MyWebMvcConfig.java
+++ src/main/java/com/okccc/config/MyWebMvcConfig.java
@@ -2,9 +2,13 @@ package com.okccc.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -26,6 +30,34 @@ public class MyWebMvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/a/", "classpath:/b/")
                 .setCacheControl(CacheControl.maxAge(3600, TimeUnit.SECONDS));
+    }
+
+    /**
+     * 处理跨域请求
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 添加路径规则
+                .allowedOriginPatterns("*")   // 允许请求来源的域规则
+                .allowedMethods("*")          // 允许的请求方法
+                .allowedHeaders("*")          // 允许的请求头
+                .allowCredentials(true);      // 是否允许在跨域的情况下传递Cookie
+    }
+
+    /**
+     * 配置拦截器
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+
+    /**
+     * 配置异常处理器
+     */
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        WebMvcConfigurer.super.configureHandlerExceptionResolvers(resolvers);
     }
 
 }
