9010c92f89be7d96d56eaee3eda2a1fa1d824575
okccc <1573976179@qq.com>
2024-07-21 10:31:37 +0800

    Field injection is not recommended

--- src/main/java/com/okccc/controller/UserController.java
+++ src/main/java/com/okccc/controller/UserController.java
@@ -4,6 +4,7 @@ import com.okccc.bean.User;
 import com.okccc.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -15,14 +16,20 @@ import java.util.List;
  * @Author: okccc
  * @Date: 2023/12/31 17:10:40
  * @Desc: 整合SSM,测试数据访问时GET请求可使用浏览器,POST/PUT/DELETE请求得使用Postman
+ *
+ * Field injection is not recommended
+ * 相较于@Autowired注解,Spring更推荐使用构造函数进行依赖注入
+ * 但是成员变量很多的话就要手写大量构造函数代码,此时可以使用lombok的@AllArgsConstructor自动生成构造函数
+ * 但是有些成员变量不需要初始化,此时可以给需要的添加final关键字将其变成常量,这样就必须初始化,然后使用lombok的@RequiredArgsConstructor
  */
 @Tag(name = "用户接口", description = "基于用户的CRUD")
 @RestController
 @RequestMapping(value = "user")
+@RequiredArgsConstructor
 public class UserController {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;  // 很少用,访问数据库都用mybatis
+//    @Autowired
+    private final JdbcTemplate jdbcTemplate;  // 很少用,访问数据库都用mybatis
 
     // http://localhost:8080/user/all
     @GetMapping(value = "all")
